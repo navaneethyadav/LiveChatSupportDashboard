fe9d049db3558c5cfe40233b0dd844231c1049a2
Navaneeth <navaneethkaku@gmail.com>
2026-05-13 00:45:20 +0530

    added auto logout session handling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,57 @@
 import axios from "axios"
 
+
 const API = axios.create({
+
   baseURL: "https://livechatsupportdashboard.onrender.com"
+
 })
+
+
+API.interceptors.request.use(
+
+  (config) => {
+
+    const token = localStorage.getItem(
+      "token"
+    )
+
+    if (token) {
+
+      config.headers.Authorization =
+        `Bearer ${token}`
+    }
+
+    return config
+  }
+
+)
+
+
+API.interceptors.response.use(
+
+  (response) => response,
+
+  (error) => {
+
+    if (
+      error.response &&
+      error.response.status === 401
+    ) {
+
+      localStorage.removeItem("token")
+
+      localStorage.removeItem("role")
+
+      localStorage.removeItem("full_name")
+
+      window.location.href = "/"
+    }
+
+    return Promise.reject(error)
+  }
+
+)
+
 
 export default API
